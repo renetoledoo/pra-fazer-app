ea19ff280a0237065d94f171801952f1beb2d324
Renê Braga Toledo <221.14.029@EDU.gruposete.local>
2024-03-25 19:29:34 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.form}>
+       <View style={styles.formLogo}>
+       <Image style={styles.logo} source={require('./assets/logo-pra-fazer.png')}/>
+       </View>
+      
+      <TextInput style={styles.btnEntrada} placeholder='E-mail'>
+      </TextInput>
+      
+      <TextInput style={styles.btnEntrada} placeholder='Senha'>
+      </TextInput>
+
+      <TouchableOpacity style={styles.btnEntrar} >
+        <Text style={styles.btnText}>Entrar</Text>
+      </TouchableOpacity>
+        
+         <Text style={styles.btnTextSecundario}>Criar Usuário</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +28,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FF6600',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  form: {
+    padding: 20,
+    width: '97%'
+  },
+
+  btnEntrada: {
+    backgroundColor: '#FFF',
+    padding: 15,
+    marginTop: 20,
+    borderRadius: 10
+  },
+  formLogo: {
+    alignItems: 'center',
+    marginBottom: 20
+   },
+
+   btnEntrar: {
+    backgroundColor: '#070A52',
+    borderRadius: 10,
+    padding: 15,
+    marginTop: 20,
+   },
+   btnText: {
+     color: '#FFF',
+     fontSize: 20,
+     textAlign: 'center'
+   },
+
+   btnTextSecundario: {
+    marginTop: 20,
+    color: '#FFF',
+    fontSize: 14,
+    textAlign: 'center'
+   }
 });
